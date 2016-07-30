e13cae38c096870b7f0667e5d954475ffd0ae719
Jason Noble <github+jasonn@jasonnoble.org>
2016-07-29 18:54:19 -0600

    End of class

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'capybara'
   gem 'shoulda-matchers'
   gem 'launchy'
+  gem 'faker'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   resources :users,
     only: [:new, :create],
     path_names: { new: 'signup' }
+
+  get 'login' => 'sessions#login'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
